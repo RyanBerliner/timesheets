1c34b31e9911620bfe422cac845e49fa6e95caa4
Ryan Berliner <ryan.berliner@gmail.com>
2021-07-17 17:52:49 -0400

    possible fix of service worker

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 'use strict';
 
 // https://developers.google.com/web/ilt/pwa/introduction-to-service-worker
-// if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker.register('service-worker.js')
-//   .then(function(registration) {
-//     console.log('Registration successful, scope is:', registration.scope);
-//   })
-//   .catch(function(error) {
-//     console.log('Service worker registration failed, error:', error);
-//   });
-// }
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js', {scope: '/timesheets/'})
+  .then(function(registration) {
+    console.log('Service worker registration successful, scope is:', registration.scope);
+  })
+  .catch(function(error) {
+    console.log('Service worker registration failed, error:', error);
+  });
+}
 
 const e = React.createElement;
 
--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,22 @@
 // https://web.dev/offline-cookbook/
 
-const CACHED_ASSETS = [
+const CACHED_FILES = [
   'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css',
   'https://unpkg.com/react@17/umd/react.production.min.js',
   'https://unpkg.com/react-dom@17/umd/react-dom.production.min.js',
   'https://cdn.jsdelivr.net/npm/bootstrap@5/dist/js/bootstrap.bundle.min.js',
   'https://cdn.jsdelivr.net/npm/bs5-react-elements@0/dist/bs5-react-elements.min.js',
   'app.js',
+  'index.html',
+  'manifest.json',
+  'icons-192.png',
+  'icons-512.png',
 ];
 
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open('assets-static-v1').then(function (cache) {
-      return cache.addAll(CACHED_ASSETS);
+      return cache.addAll(CACHED_FILES);
     }),
   );
 });
