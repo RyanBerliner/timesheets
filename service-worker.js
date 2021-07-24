2ce462e135f7785ccf656f6ccfd696ecd931604a
Ryan Berliner <ryan.berliner@gmail.com>
2021-07-23 22:06:48 -0400

    clear old cache on service worker activate

--- service-worker.js
+++ service-worker.js
@@ -15,12 +15,34 @@ const CACHED_FILES = [
 
 self.addEventListener('install', function (event) {
   event.waitUntil(
-    caches.open('assets-static-v1').then(function (cache) {
+    caches.open('assets-static-v2').then(function (cache) {
       return cache.addAll(CACHED_FILES);
     }),
   );
 });
 
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames
+          .filter(function (cacheName) {
+            return [
+              'assets-static-v1',
+            ].indexOf(cacheName) >= 0;
+          })
+          .map(function (cacheName) {
+            return caches.delete(cacheName);
+          }),
+      );
+    }),
+  );
+});
+
 self.addEventListener('fetch', function (event) {
-  event.respondWith(caches.match(event.request));
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    }),
+  );
 });
