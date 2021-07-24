24817ba94556971bc32a84ace60667d65168c5ca
Ryan Berliner <ryan.berliner@gmail.com>
2021-07-23 21:15:13 -0400

    move help link out of react app

--- app.js
+++ app.js
@@ -463,7 +463,6 @@ function App(props) {
       }),
       timesheets.archivedTimesheets.length === 0 ? e('li', {}, 'No archived timesheets') : null
     ),
-    e('p', {}, e('a', {href: 'https://github.com/RyanBerliner/timesheets/blob/master/README.md'}, 'What is this and how do I use it?'))
   );
 }
 
